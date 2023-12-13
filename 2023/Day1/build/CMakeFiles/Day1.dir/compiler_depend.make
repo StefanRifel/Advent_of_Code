55b03aa1d36b9e97d1a2e27ebb61b6629628684f
Stefan Rifel <96446569+StefanRifel@users.noreply.github.com>
2023-12-13 11:04:54 +0100

    Delete 2023/Day1/build directory

--- 2023/Day1/build/CMakeFiles/Day1.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Day1.
-# This may be replaced when dependencies are built.
--- 2023/Day1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xexeed/Projekts/Advent_of_Code/2023/Day1
-# Build directory: /home/xexeed/Projekts/Advent_of_Code/2023/Day1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
